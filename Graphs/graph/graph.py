9c4ec8b2b8bd2f1b38252cddd7c1cc09e825e4fd
Daniel-Ibarrola <dis96051@gmail.com>
2022-06-01 11:01:51 -0500

    Adjacency list graph supports basic graph operations, number of connected components and is path between algorithm

---
+++ Graphs/graph/graph.py
@@ -0,0 +1,109 @@
+
+
+class InvalidNodeError(ValueError):
+    pass
+
+
+class Graph:
+    """ Graph class using an adjacency list representation.
+    """
+    def __init__(self, num_nodes: int = 0):
+        self._num_nodes = num_nodes
+        self._num_edges = 0
+        self._adjacency_list = [[] for _ in range(num_nodes)]
+
+    def _validate_node(self, node: int) -> None:
+        if node < 0 or node >= self._num_nodes:
+            raise InvalidNodeError(f"{node} is not a node of this graph")
+
+    @property
+    def num_nodes(self) -> int:
+        """ Returns the number of nodes in the graph. """
+        return self._num_nodes
+
+    @property
+    def num_edges(self) -> int:
+        """ Returns the number of edges in the graph. """
+        return self._num_edges
+
+    @property
+    def adjacency_list(self) -> list[list[int]]:
+        """ Returns the adjacency list of the graph. """
+        return self._adjacency_list
+
+    def add_edge(self, node_1: int, node_2: int) -> None:
+        """ Add an edge between the given nodes to the graph. """
+        if not self.is_neighbor(node_1, node_2):
+            self._adjacency_list[node_1].append(node_2)
+            self._adjacency_list[node_2].append(node_1)
+            self._num_edges += 1
+
+    def add_node(self) -> None:
+        """ Adds a node to the graph. """
+        self.adjacency_list.append([])
+        self._num_nodes += 1
+
+    def is_neighbor(self, node_1: int, node_2: int) -> bool:
+        """ Returns true uf the given nodes are neighbors. """
+        self._validate_node(node_1)
+        self._validate_node(node_2)
+
+        if node_2 in self._adjacency_list[node_1]:
+            return True
+        return False
+
+    def neighbors(self, node: int) -> list[int]:
+        """ Returns the neighbors of the given node"""
+        self._validate_node(node)
+        return self._adjacency_list[node]
+
+    def _path_between_util(self, current_node: int, end_node: int,
+                           visited: list[bool]) -> bool:
+        """ Util method to find if there is a path between two nodes
+            using a recursive algorithm.
+        """
+        if current_node == end_node:
+            return True
+
+        is_reachable = False
+        visited[current_node] = True
+
+        for neighbor in self.adjacency_list[current_node]:
+            if not visited[neighbor]:
+                is_reachable = self._path_between_util(neighbor, end_node, visited)
+                if is_reachable:
+                    break
+
+        return is_reachable
+
+    def path_between(self, start_node: int, end_node: int) -> bool:
+        """ Returns true if there is a path between the given nodes. """
+        self._validate_node(start_node)
+        self._validate_node(end_node)
+
+        visited = [False] * self.num_nodes
+        return self._path_between_util(start_node, end_node, visited)
+
+    def _explore(self, current_node: int, visited: list[bool]) -> None:
+        """ Explore the nodes reachable from the given node.
+        """
+        visited[current_node] = True
+        for neighbor in self.adjacency_list[current_node]:
+            if not visited[neighbor]:
+                self._explore(neighbor, visited)
+
+    def num_connected_components(self) -> int:
+        """ Returns the number of connected components.
+
+            Performs a depth first search while counting the number
+            of connected components.
+        """
+        visited = [False] * self._num_nodes
+        connected_components = 0
+
+        for node in range(0, self._num_nodes):
+            if not visited[node]:
+                self._explore(node, visited)
+                connected_components += 1
+
+        return connected_components
---
+++ Graphs/graph/test_graph.py
@@ -0,0 +1,176 @@
+from graph import Graph, InvalidNodeError
+import pytest
+
+
+def test_graph_empty_constructor():
+    g = Graph()
+    assert g.num_nodes == 0
+    assert g.num_edges == 0
+
+
+def test_graph_constructor_with_number_of_nodes():
+    g = Graph(5)
+    assert g.num_nodes == 5
+    assert g.num_edges == 0
+
+
+def test_get_adjacency_list():
+    g = Graph()
+    assert len(g.adjacency_list) == 0
+
+    g = Graph(6)
+    assert len(g.adjacency_list) == 6
+
+    g.add_edge(0, 1)
+    g.add_edge(0, 2)
+    g.add_edge(2, 3)
+    g.add_edge(4, 5)
+
+    assert len(g.adjacency_list[0]) == 2
+    assert len(g.adjacency_list[2]) == 2
+    assert len(g.adjacency_list[4]) == 1
+
+
+def test_number_of_edges_after_adding_edge():
+    g = Graph(3)
+    g.add_edge(0, 1)
+    g.add_edge(1, 2)
+    assert g.num_edges == 2
+
+
+def test_adding_repeated_edge():
+    g = Graph(3)
+    g.add_edge(0, 1)
+    g.add_edge(0, 1)
+
+    assert g.num_edges == 1
+    assert g.is_neighbor(0, 1)
+
+
+def test_is_neighbor():
+    g = Graph(3)
+    g.add_edge(0, 1)
+    g.add_edge(1, 2)
+    assert g.is_neighbor(0, 1)
+    assert g.is_neighbor(1, 0)
+    assert g.is_neighbor(1, 2)
+    assert g.is_neighbor(2, 1)
+    assert not g.is_neighbor(0, 2)
+    assert not g.is_neighbor(2, 0)
+
+
+def test_invalid_node_raises_error():
+    g = Graph(3)
+
+    with pytest.raises(InvalidNodeError):
+        g.add_edge(0, 4)
+
+    with pytest.raises(InvalidNodeError):
+        g.add_edge(5, 4)
+
+    with pytest.raises(InvalidNodeError):
+        g.is_neighbor(0, 3)
+
+    with pytest.raises(InvalidNodeError):
+        g.is_neighbor(3, 0)
+
+
+def test_add_nodes_and_edges():
+    g = Graph()
+    g.add_node()
+    g.add_node()
+
+    assert g.num_nodes == 2
+
+    g.add_edge(0, 1)
+
+    assert g.num_edges == 1
+    assert g.is_neighbor(0, 1)
+
+
+def test_get_neighbors():
+    g = Graph(5)
+    g.add_edge(0, 1)
+    g.add_edge(0, 2)
+    g.add_edge(0, 3)
+    g.add_edge(0, 4)
+    g.add_edge(2, 3)
+    g.add_edge(3, 4)
+
+    assert g.neighbors(0) == [1, 2, 3, 4]
+    assert g.neighbors(1) == [0]
+    assert g.neighbors(2) == [0, 3]
+    assert g.neighbors(3) == [0, 2, 4]
+    assert g.neighbors(4) == [0, 3]
+
+
+@pytest.fixture
+def c4_graph() -> Graph:
+    """ Returns the C4 graph. """
+    c4 = Graph(4)
+    c4.add_edge(0, 1)
+    c4.add_edge(0, 2)
+    c4.add_edge(1, 3)
+    c4.add_edge(2, 3)
+    return c4
+
+
+@pytest.fixture
+def graph_with_two_connected_components() -> Graph:
+    """ Returns a graph with 4 nodes and two connected components"""
+    graph = Graph(4)
+    graph.add_edge(0, 1)
+    graph.add_edge(1, 2)
+    return graph
+
+
+@pytest.fixture
+def graph_with_three_connected_components() -> Graph:
+    """ Returns a graph with 8 nodes and three connected components
+    """
+    graph = Graph(8)
+    graph.add_edge(0, 1)
+    graph.add_edge(0, 2)
+    graph.add_edge(1, 2)
+    graph.add_edge(3, 4)
+    graph.add_edge(5, 6)
+    graph.add_edge(6, 7)
+    return graph
+
+
+def test_path_between(c4_graph,
+                      graph_with_two_connected_components,
+                      graph_with_three_connected_components):
+    assert c4_graph.path_between(0, 0)
+    assert c4_graph.path_between(0, 1)
+    assert c4_graph.path_between(0, 2)
+    assert c4_graph.path_between(0, 3)
+
+    graph_2 = graph_with_two_connected_components
+    assert not graph_2.path_between(0, 3)
+    assert not graph_2.path_between(1, 3)
+    assert graph_2.path_between(0, 2)
+
+    graph_3 = graph_with_three_connected_components
+    assert graph_3.path_between(5, 7)
+    assert not graph_3.path_between(6, 2)
+    assert not graph_3.path_between(6, 3)
+
+
+def test_number_of_connected_components(c4_graph,
+                                        graph_with_two_connected_components,
+                                        graph_with_three_connected_components):
+    # Empty graph has 0 connected components
+    g = Graph()
+    assert g.num_connected_components() == 0
+
+    # A graph with no edges has as many connected components as it has nodes
+    g = Graph(20)
+    assert g.num_connected_components() == 20
+
+    assert c4_graph.num_connected_components() == 1
+
+    assert graph_with_two_connected_components.num_connected_components() == 2
+
+    assert graph_with_three_connected_components.num_connected_components() == 3
+

44a3ed692ed056ecfc8ce7e214878b4a43d7c7bb
Daniel-Ibarrola <dis96051@gmail.com>
2022-06-29 19:00:15 -0500

    Function to get a dictionary of the sorted positions of a string

--- Strings/burrow_wheeler/inverse.py
+++ Strings/burrow_wheeler/inverse.py
@@ -1,5 +1,4 @@
 
-
 def sorted_positions_dict(string: str) -> dict[int, int]:
     """ Returns a dictionary with the indexes of the original string
         as keys, and the indexes of the sorted string as value.
@@ -10,13 +9,18 @@ def sorted_positions_dict(string: str) -> dict[int, int]:
             >>> {0: 1, 1: 0, 2 : 3, 3: 2}
     """
     sorted_indices = sorted(range(len(string)), key=lambda k: string[k])
-    sorted_positions = {}
-    for ii, jj in enumerate(sorted_indices):
-        sorted_positions[ii] = jj
-    return sorted_positions
+    return {
+        ii: jj for ii, jj in enumerate(sorted_indices)
+    }
 
 
 def inverse_burrow_wheeler_transform(transform: str) -> str:
     """ Returns the inverse Burrow-Wheeler transform. """
-    pass
 
+    sorted_positions = sorted_positions_dict(transform)
+    inverse = "$"
+    for ii in range(len(sorted_positions)):
+        pass
+
+    assert len(inverse) == len(transform), "inverse and transform aren't the same size"
+    return inverse[::-1]
--- Strings/burrow_wheeler/test_bw.py
+++ Strings/burrow_wheeler/test_bw.py
@@ -6,6 +6,17 @@ def test_sorted_positions_dict():
     positions_expected = {0: 1, 1: 0, 2 : 3, 3: 2}
     assert sorted_positions_dict("badc") == positions_expected
 
+    positions_expected = {
+        0: 6,
+        1: 0,
+        2: 4,
+        3: 5,
+        4: 1,
+        5: 2,
+        6: 3,
+    }
+    assert sorted_positions_dict("AGGGAA$") == positions_expected
+
 
 def test_inverse_burrow_wheeler_transform():
 

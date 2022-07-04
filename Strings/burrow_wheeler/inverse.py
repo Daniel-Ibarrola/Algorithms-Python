85261219f63a6df032a429390dc3c79126633373
Daniel-Ibarrola <dis96051@gmail.com>
2022-07-04 16:25:23 -0500

    Inverse burrow wheelers trasnform passes tests.

--- Strings/burrow_wheeler/inverse.py
+++ Strings/burrow_wheeler/inverse.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 
 def sorted_positions_dict(string: str) -> dict[int, int]:
     """ Returns a dictionary with the indexes of the original string
@@ -14,13 +16,24 @@ def sorted_positions_dict(string: str) -> dict[int, int]:
     }
 
 
+def sorted_positions_array(string: str) -> np.ndarray:
+    """ Returns an array with the indices that each character
+        would have in the sorted string
+    """
+    sorted_indices = [ii[0] for ii in sorted(enumerate(string), key=lambda x: x[1])]
+    return np.argsort(sorted_indices)
+
+
 def inverse_burrow_wheeler_transform(transform: str) -> str:
     """ Returns the inverse Burrow-Wheeler transform. """
 
-    sorted_positions = sorted_positions_dict(transform)
-    inverse = "$"
-    for ii in range(len(sorted_positions)):
-        pass
+    sorted_positions = sorted_positions_array(transform)
+    inverse = ""
+    next_index = 0
 
-    assert len(inverse) == len(transform), "inverse and transform aren't the same size"
-    return inverse[::-1]
+    for _ in range(0, len(transform) - 1):
+        inverse += transform[next_index]
+        next_index = sorted_positions[next_index]
+
+    inverse = inverse[::-1]
+    return inverse + '$'
--- Strings/burrow_wheeler/test_bw.py
+++ Strings/burrow_wheeler/test_bw.py
@@ -1,4 +1,6 @@
 from inverse import sorted_positions_dict, inverse_burrow_wheeler_transform
+from inverse import sorted_positions_array
+import numpy as np
 
 
 def test_sorted_positions_dict():
@@ -16,6 +18,12 @@ def test_sorted_positions_dict():
         6: 3,
     }
     assert sorted_positions_dict("AGGGAA$") == positions_expected
+
+
+def test_sorted_positions_array():
+
+    positions_expected = np.array([13, 8, 9, 12, 7, 10, 11, 1, 2, 3, 4, 5, 0, 6])
+    assert np.all(sorted_positions_array("smnpbnnaaaaa$a") == positions_expected)
 
 
 def test_inverse_burrow_wheeler_transform():

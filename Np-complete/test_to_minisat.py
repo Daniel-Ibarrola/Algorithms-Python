154a5eea5a88e97fa881481e312a56207c040ba8
Daniel-Ibarrola <dis96051@gmail.com>
2022-08-25 11:01:13 -0500

    to minsat functions

---
+++ Np-complete/test_to_minisat.py
@@ -0,0 +1,49 @@
+from to_minisat import to_minisat, write_minisat_file
+import os
+
+
+def test_write_minisat_file():
+    n_variables = 2
+    formula = [
+        [1, 2, 0],
+        [1, -2, 0],
+        [-1, -2, 0]
+    ]
+    write_minisat_file(formula, n_variables, "temp.cnt")
+    with open("temp.cnt") as fp:
+        lines = []
+        for line in fp.readlines():
+            lines.append(line)
+
+    os.remove("temp.cnt")
+
+    assert len(lines) == 4
+    assert lines[0].strip() == "p cnf 2 3"
+    assert lines[1].strip() == "1 2 0"
+    assert lines[2].strip() == "1 -2 0"
+    assert lines[3].strip() == "-1 -2 0"
+
+
+def test_to_minisat_satisfiable_formula():
+
+    n_variables = 2
+    formula = [
+        [1, 2, 0],
+        [1, -2, 0],
+        [-1, -2, 0],
+    ]
+    result = to_minisat(formula, n_variables)
+    assert "SAT" in result
+
+
+def test_to_minisat_unsatisfiable_formula():
+    n_variables = 2
+    formula = [
+        [1, 2, 0],
+        [1, -2, 0],
+        [-1, -2, 0],
+        [-1, 2, 0],
+    ]
+    result = to_minisat(formula, n_variables)
+
+    assert "UNSAT" in result
---
+++ Np-complete/to_minisat.py
@@ -0,0 +1,37 @@
+import os
+
+
+def write_minisat_file(formula: list[list[int]],
+                       n_variables: int,
+                       name: str) -> None:
+    """ Writes a file with a boolean formula in CNF that can be
+        read by the minisat program to check its satisfiability.
+    """
+    n_clauses = len(formula)
+    with open(name, "w") as fp:
+        # Write the header
+        fp.write(f"p cnf {n_variables} {n_clauses}\n")
+        # Write each clause
+        for clause in formula:
+            line = " ".join([str(num) for num in clause])
+            line += "\n"
+            fp.write(line)
+
+
+def to_minisat(formula: list[list[int]],
+               n_variables: int) -> str:
+    """ Returns a string with the results of the given formula
+        in the minisat program.
+    """
+
+    formula_file = "./tmp.cnt"
+    write_minisat_file(formula, n_variables, formula_file)
+    result_file = "./tmp.sat"
+    os.system(f"minisat {formula_file} {result_file}")
+
+    with open(result_file) as fp:
+        result = fp.read()
+
+    os.remove(formula_file)
+    os.remove(result_file)
+    return result
